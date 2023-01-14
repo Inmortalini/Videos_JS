0e02341d875b95656ecb60794a22751a0aadbba3
Pedro Barrientos Voss <pedrobarrientosvoss@gmail.com>
2023-01-14 11:18:47 -0400

    Agregamos boton de mute/unmute

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,24 +1,44 @@
-function mediaPlayer(config) {
-  this.media = config.el;
+function MediaPlayer(config) {
+  this.Media = config.el;
+  this.plugins = config.plugins || [];
+  this._initPLugins();//Inicializacion
 }
-
-mediaPlayer.prototype.play = function() {
-  this.media.play();
+//funcion que no inicia el autoRun de nuestros plugins(en este caso solo tenemos uno)
+MediaPlayer.prototype._initPLugins = function() {
+    this.plugins.forEach(element => {
+        element.run(this)
+    });
+  };
+  //funcion mute para que se calle el video antes de empezar
+  MediaPlayer.prototype.mute = function() {
+    this.Media.muted=true;
+  };
+  //muteUnmute Function
+  MediaPlayer.prototype.unmuteMute = function() {
+    if(this.Media.muted){
+        this.Media.muted=false
+    }
+    else{
+        this.Media.muted=true
+    }
+  };
+MediaPlayer.prototype.play = function() {
+  this.Media.play();
 };
 
-mediaPlayer.prototype.pause = function() {
-  this.media.pause();
+MediaPlayer.prototype.pause = function() {
+  this.Media.pause();
 };
 
-mediaPlayer.prototype.togglePlay = function() {
-  if (this.media.paused) {
+MediaPlayer.prototype.togglePlay = function() {
+  if (this.Media.paused) {
     this.play();
   } else {
     this.pause();
   }
 };
 
-export default mediaPlayer;
+export default MediaPlayer;
 // export const temp1 = "Este es un texto ficticio1";
 // export const temp2 = "Este es un texto ficticio2";
 
--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,15 @@
-import mediaPlayer from "./MediaPlayer.js";
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./AutoPlay.js"
 
-// import { temp1, temp2 } from './MediaPlayer.js';
-// console.log(temp1)
-// console.log(mediaPlayer);
 
 const video = document.querySelector("video");
-const player = new mediaPlayer({ el: video });
-
-const button = document.querySelector("button");
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay()],
+});
+//playPause
+const button = document.querySelector("#playPause");
 button.onclick = () => player.togglePlay();
-
-function counter() {
-  let count = 0;
-  console.log("count")
-  return{
-    incremento:function(n){
-        count++;
-    },
-    decremento:function(n){count--;},
-    getCount:function(){  console.log(count)}
-  }}
-var clousere=counter();
-clousere(5)
+//MuteUnmute
+const muteUnmute = document.querySelector("#unmuteMute");
+muteUnmute.onclick=()=>player.unmuteMute();
